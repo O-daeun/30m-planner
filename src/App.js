58e42ad5b2f36ee3bb22f0320e98921f0c037d66
daeun <qzsz797979@gmail.com>
2024-03-19 23:27:38 +0900

    하루 3등분 기능 틀 잡음 - table 생성, 예시 작성

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>하루 3등분 Planning</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>하루 3등분</th>
+            <th>일정계획</th>
+            <th>일정메모</th>
+            <th>실제</th>
+            <th>평가</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th>06:00 ~ 12:00</th>
+            <td><input type="checkbox" />위워크 가서 공부</td>
+            <td><input type="checkbox" />11시 팀미팅</td>
+            <td></td>
+            <td>계획한 대로 함</td>
+          </tr>
+          <tr>
+            <th>12:00 ~ 18:00</th>
+            <td><input type="checkbox" />위워크 공부, 집으로</td>
+            <td><input type="checkbox" />15시 매니저님 상담</td>
+            <td></td>
+            <td>조금 딴 짓을 많이 함</td>
+          </tr>
+          <tr>
+            <th>18:00 ~ 24:00</th>
+            <td><input type="checkbox" />저녁 식사, 풋살</td>
+            <td><input type="checkbox" />20-22시 풋살</td>
+            <td></td>
+            <td>시간 짬 날 때 개인 프로젝트하자</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
-
-export default App;
